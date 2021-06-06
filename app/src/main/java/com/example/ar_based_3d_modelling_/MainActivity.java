a8383f1e41e23678ace422cc3d53decef648289f
Neel456 <neelhuzurbazar@gmail.com>
2021-06-06 11:33:58 +0530

    First commit

---
+++ app/src/main/java/com/example/ar_based_3d_modelling_/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.ar_based_3d_modelling_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img=findViewById(R.id.img);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent Int=new Intent(MainActivity.this,com.example.ar_based_3d_modelling_.trial.class);
+                startActivity(Int);
+            }
+        });
+    }
+}
+//    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+//            Log.e(TAG, "Sceneform requires Android N or later");
+//            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+//            activity.finish();
+//            return false;
+//        }
+//        String openGlVersionString =
+//                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+//                        .getDeviceConfigurationInfo()
+//                        .getGlEsVersion();
+//        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+//            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+//            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+//                    .show();
+//            activity.finish();
+//            return false;
+//        }
+//        return true;
+//    }
